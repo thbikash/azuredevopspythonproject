ad3f705d43b4e09ad2114e534c5d36f600696cad
Bikash Thapa <bikashthazure2025@gmail.com>
2025-08-11 15:04:29 -0400

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db.init_app(app)
 @app.route("/")
 def home():
     if "user_id" in session:
-        return f'<h1>Welcome, User ID: {session["user_id"]}!</h1><a href="/logout">Logout</a>'
+        return f'<h1>Welcome, User ID: {session["user_id"]}!, email</h1><a href="/logout">Logout</a>'
     return redirect("/login")
 
 @app.route("/login", methods=["GET", "POST"])
